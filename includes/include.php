fa77c27e70da86f0af799c575d9d6f14a56df642
the42ndturtle <the42ndturtle@gmail.com>
2018-05-05 23:27:40 -0400

    more mail

--- includes/include.php
+++ includes/include.php
@@ -1,7 +1,7 @@
 <?php
   // include($_SERVER['DOCUMENT_ROOT']."/driverApp/includes/include.php");
-  require($_SERVER['DOCUMENT_ROOT']."/driverApp/PHPMailer-master/src/PHPMailer.php");
-  require($_SERVER['DOCUMENT_ROOT']."/driverApp/PHPMailer-master/src/SMTP.php");
+  require($_SERVER['DOCUMENT_ROOT']."/driverapp/PHPMailer-master/src/PHPMailer.php");
+  require($_SERVER['DOCUMENT_ROOT']."/driverapp/PHPMailer-master/src/SMTP.php");
   $server = "localhost";
   $username = "root";
   $password = "";
--- signup/signup.php
+++ signup/signup.php
@@ -1,5 +1,5 @@
 <?php
-  include($_SERVER['DOCUMENT_ROOT']."/driverApp/includes/include.php");
+  include($_SERVER['DOCUMENT_ROOT']."/driverapp/includes/include.php");
   $email = mysqli_real_escape_string($conn, $_POST['email']);
 
   $mail = new PHPMailer\PHPMailer\PHPMailer();
@@ -15,7 +15,7 @@
   $mail->Password = "xxxx";
   $mail->SetFrom("the42ndturtle@gmail.com");
   $mail->Subject = "Test";
-  $mail->Body = "hello";
+  $mail->Body = "<h1 style="color: blue;">try this <em>for</em> size</h1>";
   $mail->AddAddress($email);
 
    if(!$mail->Send()) {
